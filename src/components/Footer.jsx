7bbb81da248d261639c4ace55337ab301d9bf479
ssherp <ssherpa89@gmail.com>
2023-08-29 15:57:59 -0700

    started and app ap

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <div>
+            <a>
+                <h5>Linkedin</h5>
+            </a>
+            <a>
+                <h5>Medium</h5>
+            </a>
+            <a>
+                <h5>Github</h5>
+            </a>
+        </div>
+    );
+}
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar (){
+    return(
+        <div>
+            <h2>about</h2>
+            <h2>contact</h2>
+            <h2>resume</h2>
+        </div>
+    );
+}
+export default Navbar;
